d779849f39949d7e071fc9910fc75fdc25b63a3a
Finn Stolle <finn.stolle@cp.ag>
2020-08-19 20:39:50 +0200

    first commit Deleted all rubbisch

--- src/app/components/pages/home3/home3.component.ts
+++ src/app/components/pages/home3/home3.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-home3',
   templateUrl: './home3.component.html',
-  styleUrls: ['./home3.component.css']
+  styleUrls: ['./home3.component.css'],
+
 })
+
 export class Home3Component implements OnInit {
 
   constructor() { }
